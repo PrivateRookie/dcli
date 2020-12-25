32c8f4f5fc951f40e0a06a5950e9a412303e8bab
PrivateRookie <996514515@qq.com>
2020-12-25 11:39:16 +0800

    feat: support openapiv3

--- src/query.rs
+++ src/query.rs
@@ -1,5 +1,13 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+};
 
+use indexmap::IndexMap;
+use openapiv3::{
+    Contact, Info, MediaType, OpenAPI, Operation, PathItem, Paths, ReferenceOr, RequestBody,
+    Response, Responses, Server,
+};
 use serde::{Deserialize, Serialize};
 use warp::path::FullPath;
 
@@ -11,6 +19,35 @@ pub struct Query {
     pub profile: String,
     pub sql: String,
     pub url: String,
+    pub description: Option<String>,
+}
+
+impl Query {
+    pub fn open_api(&self) -> PathItem {
+        let mut get = Operation::default();
+        get.operation_id = Some(self.name.clone());
+        get.summary = self.description.clone();
+        let mut content = IndexMap::new();
+        content.insert("application/json".to_string(), MediaType::default());
+        get.request_body = Some(ReferenceOr::Item(RequestBody {
+            description: self.description.clone(),
+            content: content.clone(),
+            required: false,
+        }));
+        get.responses = Responses {
+            default: Some(ReferenceOr::Item(Response {
+                description: "OK".to_string(),
+                headers: IndexMap::new(),
+                content: content,
+                links: IndexMap::new(),
+            })),
+            responses: IndexMap::new(),
+        };
+        PathItem {
+            get: Some(get),
+            ..Default::default()
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -56,7 +93,52 @@ impl QueryPlan {
             profile: "None".to_string(),
             sql: "".to_string(),
             url: "_meta".to_string(),
+            description: Some("Meta data api".to_string()),
         });
         copied
+    }
+
+    pub fn open_api(&self) -> OpenAPI {
+        let openapi = "3.0.3".to_string();
+        let info = Info {
+            title: "DCli-HTTP".to_string(),
+            description: Some("Serve by DCli".to_string()),
+            terms_of_service: None,
+            contact: Some(Contact {
+                name: Some("PrivateRookie".to_string()),
+                url: Some("https://github.com/PrivateRookie".to_string()),
+                email: Some("xdsailfish@gmail.com".to_string()),
+            }),
+            license: None,
+            version: "3.0.0".to_string(),
+        };
+        let server = Server {
+            url: format!("/{}", self.prefix),
+            description: None,
+            variables: None,
+        };
+        let servers = vec![server];
+        let mut paths = IndexMap::new();
+        for query in self.queries.iter() {
+            paths.insert(
+                format!("/{}", query.url),
+                ReferenceOr::Item(query.open_api()),
+            );
+        }
+
+        let components = None;
+        let security = vec![];
+        let tags = vec![];
+        let external_docs = None;
+        OpenAPI {
+            openapi,
+            info,
+            servers,
+            paths,
+            components,
+            security,
+            tags,
+            external_docs,
+        }
     }
 }
